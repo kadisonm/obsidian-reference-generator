7e8a02e1d6e6d1a4b41100db6d4644eeae24d39c
Kadison <134670047+KadisonM@users.noreply.github.com>
2023-11-21 00:09:31 +1030

    Set up project

---
+++ src/main.ts
@@ -0,0 +1,72 @@
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface ReferenceGeneratorSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: ReferenceGeneratorSettings = {
+	mySetting: 'default'
+}
+
+export default class ReferenceGeneratorPlugin extends Plugin {
+	settings: ReferenceGeneratorSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		// This adds a simple command that can be triggered anywhere
+		this.addCommand({
+			id: 'open-sample-modal-simple',
+			name: 'Open sample modal (simple)',
+			callback: () => {
+				
+			}
+		});
+
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: 'sample-editor-command',
+			name: 'Sample editor command',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log(editor.getSelection());
+				editor.replaceSelection('Sample Editor Command');
+			}
+		});
+
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: ReferenceGeneratorPlugin;
+
+	constructor(app: App, plugin: ReferenceGeneratorPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
