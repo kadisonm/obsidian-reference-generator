19bba5ad441909065c18698772c409e02b25b330
Kadison <134670047+KadisonM@users.noreply.github.com>
2023-12-17 15:12:28 +1030

    Set up test file but didn't start

---
+++ tests/citation-generator.test.ts
@@ -0,0 +1,15 @@
+import CitationGenerator from '../src/citation-generator';
+
+// Create engine
+describe("CitationGenerator", () => {
+    test("Create Engine", () => {
+
+        expect(true).toBe(true);
+    });
+});
+
+// Add citation
+
+// Get bibliography
+
+// Get bibliography in format
