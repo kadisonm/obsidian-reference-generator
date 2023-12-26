b0abc5e935e96439a74dfd99fcf387c11d6046c3
Kadison <134670047+KadisonM@users.noreply.github.com>
2023-12-26 21:34:26 +1030

    Remove unused bibliography file

--- tests/test-data/bibliography.ts
+++
@@ -1,4 +0,0 @@
-const bibliography = [
-    '  <div class="csl-entry"><i>Software engineering</…en.wikipedia.org/wiki/Software_engineering</div>\n',
-    '  <div class="csl-entry"><i>Software engineering</…en.wikipedia.org/wiki/Software_engineering</div>\n',
-]
